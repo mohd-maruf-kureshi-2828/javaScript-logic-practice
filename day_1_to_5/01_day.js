c23b5f2bfe7e5bf87915d99d742cf20be91b5810
Mohamed maruf kureshi <mohamedmarufkureshikureshi@gmail.com>
2025-04-17 20:50:16 +0000

    Added Logic

---
+++ day_1_to_5/01_day.js
@@ -0,0 +1,35 @@
+// 1 give number is even or odd
+
+//let num=20;
+// if(num % 2 === 0){
+//     console.log(`Even Number ${num}`);
+// }else{
+//     console.log(`Odd Number ${num}`);
+// }
+
+//2  greater of two number
+// let a=10;
+// let b=80;
+// if(a>=b){
+//     console.log(`${a} is greater than ${b}`);
+// }else{
+//     console.log(`${b} is greater than ${a}`);
+    
+// }
+
+//3 multiple of 3 and 5 FizzBuzz
+//quesion ak number input lo 3 & 5 donoka multiple ho to "fizzbuzz"
+//sirf 3 se ho to fizz 5 se hoto buzz nhi to number print hoga
+
+// let num = 20;
+// if(num % 3 === 0 && num % 5 === 0){
+//     console.log("fizzbuzz");
+    
+// }else if(num % 3 === 0){
+//     console.log("fizz");
+// }else if(num % 5 === 0){
+//     console.log("buzz");
+// }else{
+//     console.log(num);
+    
+// }
